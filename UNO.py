cee0b06a71873ed7815ed9b68050195edf12f88b
fabetabilo <fab.tabilo@duocuc.cl>
2025-05-16 17:04:37 -0400

    Segunda version

--- UNO.py
+++ UNO.py
@@ -1,6 +1,33 @@
+# git
+    # Los git son versiones
+
 
 # git init
-# Inicializa el repositorio git (.git) dentro del directorio en que se esta trabajando
-# por ejemplo, ejerciciosgit
+    # Inicializa el repositorio git (.git) dentro del directorio en que se esta trabajando
+    # por ejemplo, ejerciciosgit
+
+# git status -s
+    # Indica que pasa con este directorio y sus archivos: (??) -> no tiene "versonamiento"
+    # (??): No tiene version, nada
+    # (A): Archivo agregado 
+    # (M): (ROJO) Archivo modificado no guardado
+    # (M): (VERDE) Archivo modificado guardado
+
+# git add .
+    # (1) Agrega todo el directorio al versonamiento (.) pero tambien se puede agregar solo 1 archivo
+
+# git commit -m "Mensaje cualquiera representativo"
+    # (2) Realiza el commit 
+
+# PRIMERA VEZ CON GIT:
+#   git config --global user.email "you@example.com"    -- correo GitHub
+#   git config --global user.name "Your Name"           -- usuario GitHub
+
+# git log --oneline
+    # Muestra el ID de la version del commit
+
 
 print("UNO: Esta es la version uno")
+
+
+print("DOS: Esta es la segunda version")
